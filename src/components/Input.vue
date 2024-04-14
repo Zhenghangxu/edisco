3107358626e62183857b9122d3dd360b374bbb9f
Zhenghangxu <xujason419@outlook.com>
2024-04-13 21:17:23 -0300

    feat: inti components

---
+++ src/components/Input.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+  inputType: string
+  name: string
+  value: string
+  smallLabel?: string
+}>()
+</script>
+
+<template>
+  <div class="input-container">
+    <input :type="inputType" :name="name" :value="value" />
+    <span>{{ smallLabel }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'InputDefault'
+}
+</script>
+<style lang="scss">
+.input-container {
+  padding: 0.5rem;
+  margin: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  span {
+    color: red;
+  }
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import InputText from 'primevue/inputtext'
+import Password from 'primevue/password'
+
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+defineProps<{
+  email: string
+}>()
+</script>
+
+<template>
+  <div class="header">HEADER Lite placeholder</div>
+  <div class="container">
+    <form action="">
+      <InputText type="text" v-model="email" placeholder="StudentID/Email" />
+      <Password v-model="password" :feedback="false" />
+    </form>
+  </div>
+</template>
+
+<style lang="sass" scoped></style>
